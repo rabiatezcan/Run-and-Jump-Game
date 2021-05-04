811ad620fd04fd515929a714f779a285bce6b321
rabiatezcan <rabiatezcan@hotmail.com>
2021-05-04 16:50:46 +0300

    added condition for movement

--- Run And Jump/Assets/Scripts/MoveLeft.cs
+++ Run And Jump/Assets/Scripts/MoveLeft.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 30; 
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -9f;
+    private void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);  
+        if (!playerControllerScript.isGameOver)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(gameObject.transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
